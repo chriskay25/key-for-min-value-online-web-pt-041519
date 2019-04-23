b646db8d07873fe469aefe1c5854e6840a84f751
Chris Kay <chriskay25@gmail.com>
2019-04-23 13:53:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
 	name_hash.each do |k,v|
 		if small_value == nil
 			small_value = v 
+			small_key = k
 		elsif v < small_value
 			small_value = v 
 			small_key = k 
